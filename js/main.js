4b216189ad2452e87c8933a1385cdbc84015e9cf
1Myr <ey609817@gmail.com>
2021-10-20 20:07:44 +0300

    reload

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@ const about = document.querySelector('#about')
 const contact = document.querySelector('#contact')
 const aboutContent = document.querySelector('#about-content')
 const contactContent = document.querySelector('#contact-content')
-
+const books = document.querySelector('#books')
+const booksContent = document.querySelector('#books-content')
 about.addEventListener('click', () => {
   const aboutBox = new WinBox({
     title: 'About Me',
@@ -33,6 +34,25 @@ contact.addEventListener('click', () => {
     bottom: 50,
     left: 250,
     mount: contactContent,
+    onfocus: function () {
+      this.setBackground('#00aa00')
+    },
+    onblur: function () {
+      this.setBackground('#777')
+    },
+  })
+})
+
+books.addEventListener('click', () => {
+  const contactBox = new WinBox({
+    title: 'Books',
+    width: '400px',
+    height: '400px',
+    top: 150,
+    right: 50,
+    bottom: 50,
+    left: 250,
+    mount: booksContent,
     onfocus: function () {
       this.setBackground('#00aa00')
     },
